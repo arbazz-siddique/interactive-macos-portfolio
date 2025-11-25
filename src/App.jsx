b4f7d6fca28747d90ed3dab1294c1ee4f8e24c22
arbazz-siddique <arbazzsiddiquecse@gmail.com>
2025-11-25 10:32:14 +0530

    Implement most tedious file setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold underline text-red-600">WElcom to my web folio</h1>
+        </div>
+    )
+}
+export default App
